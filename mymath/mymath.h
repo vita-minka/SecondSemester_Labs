98bbdb1f19241642e5c9d2ba9e93919276d5c1d8
vita-minka <vvolozhenkova@gmail.com>
2025-02-17 00:35:31 +0300

    Add files via upload

---
+++ mymath/mymath.h
@@ -0,0 +1,8 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+namespace mymath {
+    int sum(int a, int b);
+}
+
+#endif //MYMATH_H
---
+++ mynumber/mynumber.h
@@ -0,0 +1,9 @@
+#ifndef MYNUMBER_H
+#define MYNUMBER_H
+
+namespace mynumber {
+    void fav();
+    void notFav();
+}
+
+#endif //MYNUMBER_H
