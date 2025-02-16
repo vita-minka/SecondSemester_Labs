1d368b828713dfa4c64a3dcfadb3661506e26616
vita-minka <vvolozhenkova@gmail.com>
2025-02-17 00:38:07 +0300

    Delete mymath directory

--- mymath/mymath.cpp
+++
@@ -1,7 +0,0 @@
-#include "mymath.h"
-
-namespace mymath {
-    int sum(int a, int b) {
-        return a + b;
-    }
-}
--- mymath/mymath.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYMATH_H
-#define MYMATH_H
-
-namespace mymath {
-    int sum(int a, int b);
-}
-
-#endif //MYMATH_H
